e71dfd562a2653ebcf3c2d8f179e2451932d873e
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-06-29 23:49:36 +0530

    Update ping.py

--- RishuMusic/plugins/tools/ping.py
+++ RishuMusic/plugins/tools/ping.py
@@ -71,10 +71,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="• ɢʀᴏᴜᴘ •", url=f"https://t.me/Ur_support07",
+                text="• ɢʀᴏᴜᴘ •", url=f"https://t.me/TheAuraNetwork",
             ),
             InlineKeyboardButton(
-                text="• ᴍᴏʀᴇ •", url=f"https://t.me/Ur_Rishu_143",
+                text="• ᴍᴏʀᴇ •", url=f"https://t.me/AuraNumbers",
             )
         ],
         [
