a0bfffb370d582e7aade785c84c3c09bdf0f22fc
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-06-29 23:15:48 +0530

    Update ccgen.py

--- RishuMusic/plugins/tools/ccgen.py
+++ RishuMusic/plugins/tools/ccgen.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/HIMANSHI_MUSIC_BOT?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/ShreyaMusicRobot?startgroup=true"),
     ],
 ]
 
