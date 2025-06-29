82906541b941494aea6b95b3dd96d19ca06ed082
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-06-29 23:58:55 +0530

    Update search.py

--- RishuMusic/plugins/tools/search.py
+++ RishuMusic/plugins/tools/search.py
@@ -123,7 +123,7 @@ async def search_(app: app, msg: Message):
         return
     except Exception as e:
         await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @iam_SACHIN")
+        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @UseSense")
         print(f"error : {e}")
         return
 
