446709a058575d318855959e30e24788fc00bd10
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-07-12 00:47:27 +0530

    Update owner.py

--- RishuMusic/plugins/bot/owner.py
+++ RishuMusic/plugins/bot/owner.py
@@ -32,14 +32,14 @@ start_txt = """
 async def start(_, msg):
     buttons = [
         [ 
-          InlineKeyboardButton("Sɪʟᴇɴᴛʜʀᴀx", url=f"https://t.me/Silenthrax")
+          InlineKeyboardButton("Aura", url=f"https://t.me/Usesense")
         ],
         [
-          InlineKeyboardButton("ＨＥＬＰ", url="https://t.me/Silenthrax"),
-          InlineKeyboardButton("ＲＥＰＯ", url="https://t.me/BESTIE_UNITE_CLUB"),
+          InlineKeyboardButton("ＨＥＬＰ", url="https://t.me/AuraVisual"),
+          InlineKeyboardButton("ＲＥＰＯ", url="https://t.me/HeavenChatGroup"),
           ],
                [
-                InlineKeyboardButton(" ＮＥＴＷＯＲＫ", url=f"https://t.me/silenthrex"),
+                InlineKeyboardButton(" ＮＥＴＷＯＲＫ", url=f"https://t.me/AuraVisual"),
 ],
 [
 InlineKeyboardButton("ＯＦＦＩＣＩＡＬ ＢＯＴ", url=f"https://t.me/RJ_92_MUSIC_BOT"),
