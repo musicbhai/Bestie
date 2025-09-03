560de2d958ca15b1bb575b30919eb3c78cd24713
musicbhai <dghffyhcyucfuhvfuhc@gmail.com>
2025-09-03 14:38:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,6 +19,11 @@ LOGGER_ID = int(getenv("LOGGER_ID", -1002754356584))
 OWNER_ID = int(getenv("OWNER_ID", None))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+
+
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/musicbhai/Bestie",)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
